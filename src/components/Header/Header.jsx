c44c85af7ade620419c097d9fc8ce4c9c66c00a7
rohan09-raj <rajrohan1914@gmail.com>
2022-07-17 23:17:33 +0530

    Built a basic folder structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = ({subheading}) => {
+  return (
+    <header className={styles.header}>
+      <h1>AADHAAR</h1>
+      <h3>{subheading}</h3>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header subheading='Mera Aadhaar Meri Pehchan' />
+    </>
+  );
+};
+
+export default Home;
