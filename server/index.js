337b0a2a7d358a597e114197cf5d12e8ed81a19e
rohan09-raj <rajrohan1914@gmail.com>
2022-07-28 19:48:13 +0530

    folder structure setup and database addition

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/health.js';
+
 const app = Express();
 dotenv.config();
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
+
+app.use('/', userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT;
